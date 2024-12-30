68a421215dcb3778056c88fa134b2f8f3727610f
Rayan Hany <rayanhanya@gmial.com>
2024-12-30 14:57:05 +0530

    models created of user & post

--- fluxify_project/fluxify_post/models.py
+++ fluxify_project/fluxify_post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from fluxify_user.models import user_name
 # Create your models here.
+class post(models.Model):
+    leve=1
+    delete=0
+    delete_choices=((live,'live')),(delete,'delete')
+    posted_by=models.ForeignKey(user_name)
+    catagory=models.CharField(max_length=100)
+    post_location=models.CharField(max_length=100)
+    post_description=models.CharField(max_length=200)
+    avg_price=models.FloatField()
+    estimate_view=models.IntegerField()
+    created_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_name
+
+
--- fluxify_project/fluxify_user/models.py
+++ fluxify_project/fluxify_user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class user(models.model):
+    mail_id=models.CharField(max_length=200)
+    user_name=models.CharField(max_length=200)
+    phone_no=models.CharField(max_length=10)
+    pin_code=models.CharField(max_length=6)
+    adress=models.TextField()
+    profile_photo=models.ImageField(upload_to='/media')
+
+    def __str__(self):
+        return self.user_name
+    
