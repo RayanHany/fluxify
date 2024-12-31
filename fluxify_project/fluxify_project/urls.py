8ae7085c77da508ca2a7e43b53501de07682b154
Rayan Hany <rayanhanya@gmial.com>
2024-12-31 20:35:46 +0530

    erors resolved in settings and urls

--- fluxify_project/fluxify_project/urls.py
+++ fluxify_project/fluxify_project/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-uelpatterns += static(settings.MEDIA_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.STATIC_ROOT)
