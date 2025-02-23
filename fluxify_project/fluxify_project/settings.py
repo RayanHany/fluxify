7d3fc8fb274f84d87ed44d14c24e3ddd122954e6
Rayan Hany <rayanhanya@gmial.com>
2025-02-24 02:52:16 +0530

    json file adedd

--- fluxify_project/fluxify_project/settings.py
+++ fluxify_project/fluxify_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!tdp8vwz%+=%#8-*3kz*!84clhm6ym3kr5)zzo*c)#xct*ekwz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- fluxify_project/fluxify_project/wsgi.py
+++ fluxify_project/fluxify_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fluxify_project.settings')
 
 application = get_wsgi_application()
+
+
+app=application
